ce98e25286aa7f96f13484d0c02a376ca354f800
Sean <sobringer@collineargroup.com>
2020-10-22 09:03:26 -0700

    add onoff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 const bodyParser = require('body-parser')
 
 // app.disable('x-powered-by')
