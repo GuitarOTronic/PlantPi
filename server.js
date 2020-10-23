d802020f35fe25b7aed14d0d5930e05bd55cf8ab
Sean <sobringer@collineargroup.com>
2020-10-23 09:25:32 -0700

    fix import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import TemperatureController from './sensors/Temperature'
+import TemperatureController from './sensors/Temperature/TemperatureController.js'
 const app = express()
 const port = process.env.PORT || 8081
 
