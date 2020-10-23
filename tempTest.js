86f78466396901c8e9b2634bfcfe28ca2b9a5265
Sean <sobringer@collineargroup.com>
2020-10-23 08:42:47 -0700

    add fahrenheit converter

--- tempTest.js
+++ tempTest.js
@@ -6,15 +6,29 @@ ds18b20.sensors(function (err, ids) {
 
 // ... async call
 ds18b20.temperature('28-0115721161ff', function (err, value) {
-  console.log('Current temperature is', value);
+  // console.log('Current temperature is', value);
 });
 
 // ... or sync call
-console.log('Current temperature is' + ds18b20.temperatureSync('28-0115721161ff'));
+// console.log('Current temperature is' + ds18b20.temperatureSync('28-0115721161ff'));
 
 // default parser is the decimal one. You can use the hex one by setting an option
 ds18b20.temperature('28-0115721161ff', { parser: 'hex' }, function (err, value) {
-  console.log('Current temperature is', value);
+  // console.log('Current temperature is', value);
 });
 
-console.log('Current temperature is' + ds18b20.temperatureSync('28-0115721161ff', { parser: 'hex' }));
+function convertCelsiusToFahrenheit(tempC) {
+  return (tempC * 9) / 5 + 32
+}
+
+function getTemperatureF() {
+  let tempC = null
+  ds18b20.temperature('28-0115721161ff', function (err, value) {
+    tempC = value
+  })
+
+  let tempF = convertCelsiusToFahrenheit(tempC)
+
+}
+
+console.log('Current temperature is' + getTemperatureF());
