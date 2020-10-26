c8fd788be0313abc4efaae295bdee55cbdbcb85b
Sean <sobringer@collineargroup.com>
2020-10-25 18:32:27 -0700

    adf

--- sensors/Temperature/TemperatureController.js
+++ sensors/Temperature/TemperatureController.js
@@ -38,6 +38,7 @@ class TemperatureController {
     let tempF
     await getTemperature()
       .then((temp) => {
+        console.log("Temp", temp)
         tempF = temp
       })
     // saves sensor data
