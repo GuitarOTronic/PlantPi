b81fa43b331366d744d3a691f28b621fe781ff3f
Sean <sobringer@collineargroup.com>
2020-11-29 12:20:53 -0800

    update column name

--- migrations/20201129122016_temperature.js
+++ migrations/20201129122016_temperature.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('temperature', (table) => {
     table.increments('id')
     table.date("date")
-    table.text("temp").defaultTo("")
+    table.text("temperature").defaultTo("")
     table.text("openWeatherTemp").defaultTo("")
   })
 };
