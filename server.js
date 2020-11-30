b0e441993d1a1faf698864e5a86a305847866ea5
Sean <sobringer@collineargroup.com>
2020-11-30 08:34:59 -0800

    adf

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json())
 app.use('/temperature', TemperatureRouter)
 
 app.use('/', (req, res, next) => {
-  const {currentTemp} = temperatureController.getTemperatureF()
+  const {currentTemp} = TemperatureController.getTemperatureF()
   res.send(home(currentTemp))
   // res.sendFile(path.join(__dirname+'/home.html'));
 })
